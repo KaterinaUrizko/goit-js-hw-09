faa9389c3966512c4a33ea0b1c3535c52c371772
Katerina Urizko <k.urizko@gmail.com>
2023-02-26 10:58:20 +0200

    promises changed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,28 @@
-refs= {
-    startBTN :document.querySelector('[data-start]'),
-    stopBTN : document.querySelector('[data-stop]'),
-    backGround: document.querySelector('body'), 
-}
+refs = {
+  startBTN: document.querySelector('[data-start]'),
+  stopBTN: document.querySelector('[data-stop]'),
+  backGround: document.querySelector('body'),
+};
 
 refs.startBTN.addEventListener('click', onChangeColor);
 refs.stopBTN.addEventListener('click', onStopColor);
 
-
 let timerInt = null;
 
-function onChangeColor () {
-
-   timerInt= setInterval(() => {
-        let color = getRandomHexColor();
-        refs.backGround.style.backgroundColor =`${color}`},1000
-);
-        refs.startBTN.disabled = true;
+function onChangeColor() {
+  timerInt = setInterval(() => {
+    let color = getRandomHexColor();
+    refs.backGround.style.backgroundColor = `${color}`;
+  }, 1000);
+  refs.startBTN.disabled = true;
 }
 
-function onStopColor () {
-        clearInterval(timerInt);
-        refs.startBTN.disabled = false;
+function onStopColor() {
+  clearInterval(timerInt);
+  refs.startBTN.disabled = false;
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 // goit-js-hw-09/01-color-switcher.js at main · KaterinaUrizko/goit-js-hw-09
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,80 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
 import Notiflix from 'notiflix';
 
 const refs = {
-  
   delay: document.querySelector("[name='delay']"),
   step: document.querySelector('[name="step"]'),
   amount: document.querySelector('[name="amount"]'),
-  submit: document.querySelector("button"),
-
+  submit: document.querySelector('button'),
 };
 
 // refs.submit.addEventListener('submit', onSubmitForm);
 
-
 refs.submit.addEventListener('click', onClickBtn);
 
-
-
 function onClickBtn(event) {
-
   event.preventDefault();
 
-    let delay = Number(refs.delay.value);
+  let delay = Number(refs.delay.value);
   const step = Number(refs.step.value);
   const amount = Number(refs.amount.value);
 
-   if (delay < 0 || step < 0 || amount <= 0) {
-        Notiflix.Notify.warning('Please choose a number more then 0');
-      
-     return;
-    } 
-   
-   
+  if (delay < 0 || step < 0 || amount <= 0) {
+    Notiflix.Notify.warning('Please choose a number more then 0');
+
+    return;
+  }
+
   for (let position = 1; position <= amount; position += 1) {
-    
-        
-        createPromise(position, delay)
+    createPromise(position, delay)
       .then(({ position, delay }) => {
-               
-          Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(` Rejected promise ${position} in ${delay}ms`);
-  });
-    
+        Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(` Rejected promise ${position} in ${delay}ms`);
+      });
+
     delay += step;
-    
-};
+  }
 }
 
-
-
 function createPromise(position, delay) {
-
-
   const shouldResolve = Math.random() > 0.3;
 
-
-        return new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-      
-          if (shouldResolve) {
-    
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
     }, delay);
   });
-};
-
+}
